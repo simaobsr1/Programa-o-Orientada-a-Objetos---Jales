df5117c75fe8b479d0084c28f8f2883de4da6f90
simaobsr1 <148493401+simaobsr1@users.noreply.github.com>
2025-09-02 18:24:09 +0000

    salvar

---
+++ ‘cap_1_quest_1’,/calcular.py
@@ -0,0 +1,12 @@
+class Calculadora:
+    def calcular_area_quadrado(self,lado):
+        return lado * lado 
+
+    def calcular_area_circulo (self,raio):
+        pi = 3.1415
+        return pi * (raio * raio )
+
+calculo = Calculadora()
+
+print ("area do quadrado",calculo.calcular_area_quadrado(5))
+print ("area do circulo",calculo.calcular_area_circulo(2))
